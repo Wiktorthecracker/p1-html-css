de0f06b6f1bb256333846ad9d785c69027f3c219
Wiktorthecracker <9020214@student.zadkine.nl>
2022-10-13 10:17:35 +0200

    randoms

---
+++ javascript/app.js
@@ -0,0 +1,14 @@
+alert("Hallo wereld!")
+
+var getal1 = 12;
+var getal2 = 4;
+var som = getal1 * getal2;
+
+var kleur = prompt("geef een Engelse kleurnaam: (red of blue", "");
+
+// kleur aanpassen
+//document.getElementById("titel").style.color = "red";
+document.getElementById("kop").style.color = kleur;
+
+// tekst aanpassen
+document.getElementById("p1").style.color = "red";
---
+++ testing stuff adn t/java tests/script.js
@@ -0,0 +1,12 @@
+function changecolor() {
+    alert("Het werkt!");
+    console.log("Het werkt!");
+}
+
+function calculator(num, num2) {
+    let answer = num + num2;
+    console.log(answer);
+    alert(answer);
+}
+
+function
