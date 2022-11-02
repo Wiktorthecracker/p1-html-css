07eb1d618f59c86ddf6e7f5bdd8c29ee38255085
Wiktorthecracker <9020214@student.zadkine.nl>
2022-11-02 15:52:39 +0100

    ouderavondcomp

---
+++ javascript/op 8/java.js
@@ -0,0 +1,11 @@
+
+<!--
+var getal1 = parseInt(window.document.getElementById("getal1").Value, 10);
+
+var getal2 = parseInt(window.document.getElementById("getal1").Value, 10);
+
+
+antwoord = getal1+getal2;
+
+-->
+
