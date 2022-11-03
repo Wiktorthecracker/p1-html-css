29d39f2a8204985692ccd28353469d739cc416c6
Wiktorthecracker <9020214@student.zadkine.nl>
2022-11-03 16:22:36 +0100

    6.1java

--- javascript/op 6/6.1/app.js
+++ javascript/op 6/6.1/app.js
@@ -1,11 +1,17 @@
-alert("Hallo wereld!")
+alert("hallo wereld!");
+ // document.getElementById("title").style.color = "red";
 
-var getal1 = 12;
-var getal2 = 4;
-var som = getal1 * getal2;
+// var getal1 = 12;
+ //var getal2 = 4;
+ //var som = getal1 * getal2;
+ //alert(getal1 + " x" +getal2 + "=" + som);
 
+ // promtbox
+ var kleur =prompt("geef een engelse kleurnaam: (red of blue", "");
 
+ // kleur aanpassen
+ document.getElementById("title").style.color = kleur;
+ //document.getElementById("title").style.color = "red";
 
-function changecolor(){
-    document.getElementById("rood").style.color = "red";
-}
+ // tekst aanpassen
+ document.getElementById("p1").innerHTML = "WOOOOOOOWWW "+ kleur +"!";
