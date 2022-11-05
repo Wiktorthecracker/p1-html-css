2009e75f873faa398a6d8ba62730b8b32e56fa4d
Wiktorthecracker <9020214@student.zadkine.nl>
2022-11-05 20:51:45 +0100

    8.2

---
+++ javascript/op 8/8.2/java.js
@@ -0,0 +1,34 @@
+function myfun()
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a+b
+
+}
+
+function myfun2() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a-b
+
+
+}
+function myfun3() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a*b
+
+
+}
+
+function myfun4() 
+{ var a=parseInt(document.getElementById('number').value);
+var b=parseInt(document.getElementById('numbers').value);
+document.getElementById('result').innerHTML=a/b
+
+
+}
+if (Number < 0) {
+    greeting = "Cool";
+} else {
+
+}
